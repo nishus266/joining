f03cb036904081ff35e8377f9dce595e785f3cfc
nishus266 <nishant.sharma.cs.2016@miet.ac.in>
2019-01-03 15:22:01 +0530

    template.js

--- template.js
+++ template.js
@@ -4,31 +4,18 @@ var fs= require('fs');
 http.createServer( function(request, response){
    var q= url.parse(request.url, true);
    var g= q.pathname;
-   console.log(q);
    var m='./views'+g+'.html';
    console.log(m);
-   if(g=='/dashboard'){
+
 
      fs.readFile(m, function(err, data) {
        if (err) {
        }
-       console.log(data);
        response.writeHead(200, {'content-Type': 'text/html'});
        response.write(data);
        response.end();
  });
-}
 
- if(g=='/login'){
-   fs.readFile(m, function(err, data) {
-     if (err) {
-     }
-     console.log(data);
-     response.writeHead(200, {'content-Type': 'text/html'});
-     response.write(data);
-     response.end();
-});
- }
 
 
 
